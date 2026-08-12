0d6a4c19e2ba51e4fb43f65498bf8b73aaf5cb3e
CodesWithManthan <manthansoni494@gmail.com>
2026-08-12 08:44:35 +0530

    fresh start

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import ChallanForm from "./pages/ChallanForm";
 import ChallanDetail from "./pages/ChallanDetail";
 import "./styles/global.css";
 
+//rebuild bug
 function ProtectedRoute({ children }: { children: ReactNode }) {
   const { token } = useAuth();
   return token ? <>{children}</> : <Navigate to="/login" replace />;
